29ca4219bfaed96adee519933dd462b44a893869
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-06-23 20:23:12 +0100

    validation completed

--- client/src/schema/index.js
+++ client/src/schema/index.js
@@ -3,7 +3,7 @@ import * as yup from 'yup'
 export const registrationSchema = yup.object().shape({
     fullName: yup
     .string()
-    .required('Required')
+    .required('This is a required field')
     .min(4, ['Pls, provide your valid credential'])
     .max(25, ['This field should not exceed 25 characters']),
     email: yup
@@ -14,12 +14,12 @@ export const registrationSchema = yup.object().shape({
     .max(25, ['Email must be not exceed 25 characters']),
     password: yup
     .string()
-    .required('Required')
+    .required('This is a required field')
     .min(4, ['Password must be a minimum of 4 characters'])
     .max(25, ['Password must be not exceed 25 characters']),
     confirmPassword:  yup
     .string()
+    .required('This is a required field')
     .oneOf([yup.ref("password"), null], 'Password and confirm password must match')
-    .required('Required')
-    
+   
 })
